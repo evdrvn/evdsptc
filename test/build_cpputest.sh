97ed94f6d1b609c2a3d2c29e910ea968ebeb527c
evdrvn <evdrvn@gmail.com>
2017-02-27 23:46:19 +0900

    use gcov

--- test/build_cpputest.sh
+++ test/build_cpputest.sh
@@ -2,7 +2,7 @@
 cd ext/cpputest
 rm -rf cpputest_build/*
 cd cpputest_build
-cmake -DCMAKE_INSTALL_PREFIX=.. # -DCOVERAGE=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=.. -DCOVERAGE=ON ..
 make 
 make install
 cd ../../..
